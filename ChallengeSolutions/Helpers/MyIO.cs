5431e8cb8fa8316dc574ba30140417abefa8fa43
dalestevenjohnson <dalestevenjohnson@gmail.com>
2021-12-01 19:59:43 +0000

    initialised solution

---
+++ ChallengeSolutions/Helpers/MyIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ChallengeSolutions.Helpers
+{
+    internal class MyIO
+    {
+        private static string _filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PuzzleInputs");
+        internal static List<string> ReadFile(int year, int day=1, int part=1)
+        {
+            var dataFile = GetDataFileName(year, day, part);
+            string[] lines = System.IO.File.ReadAllLines(Path.Combine(_filePath, dataFile);
+            return lines.ToList();
+        }
+
+        private static string GetDataFileName(int year, int day, int part)
+        {
+            return $"Y{year}D{day}P{part}.txt";
+        }
+    }
+}
---
+++ ChallengeSolutions/Y2021/Y2021D1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChallengeSolutions.Y2021
+{
+    public class Y2021D1
+    {
+    }
+}
