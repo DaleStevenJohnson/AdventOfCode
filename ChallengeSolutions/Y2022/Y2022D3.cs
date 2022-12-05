68743fa957290758df8f926d51223194d6f7535c
Dale Johnson <dalestevenjohnson@gmail.com>
2022-12-05 20:10:46 +0000

    Solved Day 3

---
+++ ChallengeSolutions/Y2022/Y2022D3.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using ChallengeSolutions.Abstractions;
+using ChallengeSolutions.Helpers;
+
+namespace ChallengeSolutions.Y2022
+{
+  
+    public class Y2022D3 : ISolution
+    {
+       
+        public List<string> TestData { get; set; }
+        public List<string> PuzzleData { get; set; }
+        public Y2022D3()
+        {
+            TestData = MyIO.ReadStringsFromFile(2022, 3, 1, true);
+            PuzzleData = MyIO.ReadStringsFromFile(2022, 3, 1, false);
+        }
+
+        public string SearchBackpackForDuplicates(string backpack)
+        {
+            int halfway = backpack.Length / 2;
+            var compartment1 = backpack.Substring(0, halfway);
+            var compartment2 = backpack.Substring(halfway);
+            var duplicates = string.Empty;
+            foreach (var item in compartment1)
+            {
+                if (compartment2.Contains(item) && !duplicates.Contains(item))
+                {
+                    duplicates += item;
+                }
+            }
+            return duplicates;
+        }
+
+        public int GetPriorityScore(string items) 
+        {
+            var total = 0;
+            for (int i = 0; i < items.Length; i++)
+            {
+                int ascii = (int)items[i];
+                if (ascii + 4 > 100)
+                {
+                    total += ascii + 4 - 100; // I know I could do -96!
+                }
+                else
+                {
+                    total += ascii - 38;
+                }
+
+            }
+            return total;
+        }
+
+        public string SolvePart1(List<string> data)
+        {
+           var total = 0;
+           foreach (var backpack in data) 
+            {
+                var duplicates = SearchBackpackForDuplicates(backpack);
+                var score =  GetPriorityScore(duplicates);
+                total += score;
+            }
+           return total.ToString();
+        }
+
+        public static string? SearchBackpacksForBadge(string backpack1, string backpack2, string backpack3)
+        {
+            foreach (var letter in backpack1)
+            {
+                if (backpack2.Contains(letter) && backpack3.Contains(letter)) return letter.ToString();
+            }
+            return null;
+        }
+
+        public string SolvePart2(List<string> data)
+        {
+            var total = 0;
+            for (int i = 0; i < data.Count; i+=3)
+            {
+                var backpack1 = data[i];
+                var backpack2 = data[i+1];
+                var backpack3 = data[i+2];
+                var badge = SearchBackpacksForBadge(backpack1, backpack2, backpack3);
+                var score = GetPriorityScore(badge);
+                total += score;
+            }
+            return total.ToString();
+        }
+    }
+}
---
+++ ChallengeTests/Y2022/Day3Tests.cs
@@ -0,0 +1,62 @@
+﻿using ChallengeSolutions.Abstractions;
+using ChallengeSolutions.Y2022;
+using NUnit.Framework;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ChallengeTests.Y2022
+{
+    internal class Day3Tests
+    {
+        [Test]
+        public void Part1SampleData()
+        {
+            ISolution day = new Y2022D3();
+            var result = day.SolvePart1(day.TestData);
+            Assert.That(result, Is.EqualTo("157"));
+        }
+        [Test]
+        public void Part1PuzzleData()
+        {
+            ISolution day = new Y2022D3();
+            var result = day.SolvePart1(day.PuzzleData);
+            Assert.That(result, Is.EqualTo("7908"));
+        }
+        [Test]
+        public void Part2SampleData()
+        {
+            ISolution day = new Y2022D3();
+            var result = day.SolvePart2(day.TestData);
+            Assert.That(result, Is.EqualTo("70"));
+        }
+        [Test]
+        public void Part2PuzzleData()
+        {
+            ISolution day = new Y2022D3();
+            var result = day.SolvePart2(day.PuzzleData);
+            Assert.That(result, Is.EqualTo("2838"));
+        }
+        [Test, Sequential]
+        public void TestPriorityScoreLowerCase([Range(97, 122)] int ascii, [Range(1, 26)] int score)
+        {
+            var day = new Y2022D3();
+            var input = ((char)ascii).ToString();
+            var result = day.GetPriorityScore(input);
+            Assert.That(result, Is.EqualTo(score));
+        }
+        [Test, Sequential]
+        public void TestPriorityScoreUpperCase([Range(65, 90)] int ascii, [Range(27, 52)] int score)
+        {
+            var day = new Y2022D3();
+            var input = ((char)ascii).ToString();
+            var result = day.GetPriorityScore(input);
+            Assert.That(result, Is.EqualTo(score));
+        }
+        [Test, Sequential]
+        public void TestBackpackSearch([Values("AbcciBAt", "BBB", "AaghyYytszzzzzzast")] string backpack, [Values("A", "B", "ats")] string expected)
+        {
+            var day = new Y2022D3();
+            var result = day.SearchBackpackForDuplicates(backpack);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
