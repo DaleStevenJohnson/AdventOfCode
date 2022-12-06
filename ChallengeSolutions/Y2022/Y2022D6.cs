8ad3dc8af33776f9c715d6cb1b090ddbed08986e
Dale Johnson <dalestevenjohnson@gmail.com>
2022-12-06 22:17:48 +0000

    Solved Day 6

---
+++ ChallengeSolutions/Y2022/Y2022D6.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text;
+using ChallengeSolutions.Abstractions;
+using ChallengeSolutions.Helpers;
+
+namespace ChallengeSolutions.Y2022
+{
+  
+    public class Y2022D6 : ISolution
+    {
+       
+        public List<string> TestData { get; set; }
+        public List<string> PuzzleData { get; set; }
+        public Y2022D6()
+        {
+            TestData = MyIO.ReadStringsFromFile(2022, 6, 1, true);
+            PuzzleData = MyIO.ReadStringsFromFile(2022, 6, 1, false);
+        }
+
+        public bool QueueHasUniqueValues(Queue<string> _queue)
+        {
+            var queue = new Queue<string>(_queue) ;
+            var count = queue.Count-1;
+            for (int i = 0; i < count; i++)
+            {
+                var item = queue.Dequeue();
+                if (queue.Contains(item))
+                    return false;
+            }
+            return true;
+        }
+
+        public string PrintQueue(Queue<string> queue)
+        {
+            var result = string.Empty;
+            foreach (var item in queue)
+            {
+                result += item;
+            }
+            return result;
+        }
+
+        public int GetPositionOfMarker(string dataStream, int markerSize)
+        {
+            
+            Queue<string> queue = new Queue<string>();
+            var count = 0;
+            foreach (var letter in dataStream)
+            {
+                if (queue.Count == markerSize)
+                {
+                    if (QueueHasUniqueValues(queue))
+                        break;
+                    else
+                        queue.Dequeue();
+                }
+                queue.Enqueue(letter.ToString());
+                count++;
+            }
+            return count;
+        }
+
+        public string SolvePart1(List<string> data)
+        {
+            var dataStream = data[0];
+            var count = GetPositionOfMarker(dataStream, 4);
+            return count.ToString();
+        }
+
+        
+
+        public string SolvePart2(List<string> data)
+        {
+            var dataStream = data[0];
+            var count = GetPositionOfMarker(dataStream, 14);
+            return count.ToString();
+        }
+    }
+}
+ 
---
+++ ChallengeTests/Y2022/Day6Tests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using ChallengeSolutions.Abstractions;
+using ChallengeSolutions.Y2022;
+using NUnit.Framework;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ChallengeTests.Y2022
+{
+    internal class Day6Tests
+    {
+        [Test]
+        public void Part1SampleData()
+        {
+            ISolution day = new Y2022D6();
+            var result = day.SolvePart1(day.TestData);
+            Assert.That(result, Is.EqualTo("6"));
+        }
+        [Test]
+        public void Part1PuzzleData()
+        {
+            ISolution day = new Y2022D6();
+            var result = day.SolvePart1(day.PuzzleData);
+            Assert.That(result, Is.EqualTo("1198"));
+        }
+        [Test]
+        public void Part2SampleData()
+        {
+            ISolution day = new Y2022D6();
+            var result = day.SolvePart2(day.TestData);
+            Assert.That(result, Is.EqualTo("19"));
+        }
+        [Test]
+        public void Part2PuzzleData()
+        {
+            ISolution day = new Y2022D6();
+            var result = day.SolvePart2(day.PuzzleData);
+            Assert.That(result, Is.EqualTo("?"));
+        }
+
+        [Test]
+        public void Part1Debug()
+        {
+            var day = new Y2022D6();
+            var dataStream = day.TestData[0];
+            Queue<string> queue = new Queue<string>();
+            var count = 0;
+            foreach (var letter in dataStream)
+            {
+                if (queue.Count == 4)
+                {
+                    if (day.QueueHasUniqueValues(queue))
+                        break;
+                    else
+                        queue.Dequeue();
+                }
+                queue.Enqueue(letter.ToString());
+                count++;
+
+                TestContext.WriteLine($"Count: {count} || Queue: {day.PrintQueue(queue)}");
+            }
+            
+            
+            Assert.That(count.ToString(), Is.EqualTo("6"));
+        }
+
+    }
+}
