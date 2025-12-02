3e636aff3b8734877660ac10623cdfeea75a6073
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2025-12-02 21:58:28 +0000

    Add client for getting puzzle input

---
+++ py/aoc/puzzle_client/__init__.py
@@ -0,0 +1,3 @@
+from .puzzle_client import PuzzleClient
+from .local_cache import LocalCache
+from .client_factory import ClientFactory
---
+++ py/aoc/puzzle_client/puzzle_client.py
@@ -0,0 +1,35 @@
+from requests import get
+from .local_cache import LocalCache
+
+class PuzzleClient:
+    """Interacts with the Advent Of Code API to get puzzle input"""
+    def __init__(self, year: int, session_id: str, local_cache: LocalCache):
+        self._session_id = session_id
+        self._year = year
+        self._local_cache = local_cache
+
+    @property
+    def headers(self):
+        return {
+            "Cookie": f"session={self._session_id}"
+        }
+
+    def get_input(self, day: int) -> str:
+        if (cached_data := self._local_cache.get(day)) is not None:
+            return cached_data
+
+        url = f"https://adventofcode.com/{self._year}/day/{day}/input"
+        response = get(url, headers=self.headers)
+        if response.status_code == 404:
+            print(f"Puzzle Not Available for Year {self._year} Day {day} ")
+            exit(1)
+        response.raise_for_status()
+
+        self._local_cache.put(day, response.text)
+        return response.text
+
+    def get_description(self, day: int):
+        url = f"https://adventofcode.com/{self._year}/day/{day}"
+        response = get(url, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
