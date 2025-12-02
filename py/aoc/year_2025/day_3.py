55dab62490d22d688878370142097ded5fd93183
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2025-12-02 22:04:51 +0000

    Initial skeleton

---
+++ py/aoc/year_2025/day_3.py
@@ -0,0 +1,9 @@
+from ..helpers import PuzzleSolver
+
+
+class Day3(PuzzleSolver):
+    def _solve_part_1(self, input_text: str) -> str:
+        pass
+
+    def _solve_part_2(self, input_text: str) -> str:
+        pass
