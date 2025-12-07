c189669db50d59b85b843a06ceec32375f7b9bff
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2025-12-07 14:07:28 +0000

    Updates to refresh locally cached puzzle input from CLI

--- py/aoc/__main__.py
+++ py/aoc/__main__.py
@@ -1,4 +1,5 @@
 from click import argument, command, option
+from pygments.lexer import default
 
 from .puzzle_client import ClientFactory
 from .helpers import PuzzleSolverFactory
@@ -8,15 +9,15 @@ from .helpers import PuzzleSolverFactory
 @argument("year", type=int)
 @argument("day", type=int)
 @option("--test-data", type=str, default=None)
-def main(session_id: str, year: int, day: int, test_data: str | None):
+@option("--refresh", is_flag=True, default=False, type=bool)
+def main(session_id: str, year: int, day: int, refresh: bool, test_data: str | None):
     ## Get Input
     input_text = test_data
     if test_data is None:
         client_factory = ClientFactory(year=year, session_id=session_id)
         client = client_factory.create()
-        input_text = client.get_input(day=day)
+        input_text = client.get_input(day=day, refresh=refresh)
 
-    # print(f"Input: {input_text}")
     ## Get Solver
     puzzle_factory = PuzzleSolverFactory(year=year)
     solver = puzzle_factory.create(day)
