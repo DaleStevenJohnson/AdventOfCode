a26c020d0d6bcf34b8769bf6ae4880dd55543ce8
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2025-12-08 13:30:06 +0000

    Added Day 8 file

---
+++ py/aoc/year_2025/day_8.py
@@ -0,0 +1,10 @@
+from ..helpers import PuzzleSolver
+
+class Day8(PuzzleSolver):
+    def _solve_part_1(self, input_text):
+        return super()._solve_part_1(input_text)
+
+    def _solve_part_2(self, input_text):
+        return super()._solve_part_2(input_text)
+
+    
